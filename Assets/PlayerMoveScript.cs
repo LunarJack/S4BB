641996426da9b2ab081b877e1442039e412f34ce
LunarJack <lcolefarrell@icloud.com>
2024-08-20 11:02:18 -0700

    stuff

--- Assets/PlayerMoveScript.cs
+++ Assets/PlayerMoveScript.cs
@@ -9,6 +9,8 @@ public class PlayerMoveScript : MonoBehaviour
     public float runSpeed;
     public float jumpSpeed;
     public bool isTurnedLeft = false;
+    public bool isInAir = false;
+    public double speed = 2.0;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +20,7 @@ public class PlayerMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D) | Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D) && isInAir == false | Input.GetKey(KeyCode.RightArrow) && isInAir == false)
         {
             myRigidbody.velocity = Vector2.right * runSpeed;
             if (isTurnedLeft == true)
@@ -26,15 +28,20 @@ public class PlayerMoveScript : MonoBehaviour
                 mySpriteRenderer.flipX = false;
             }
         }
-        if (Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A) && isInAir == false | Input.GetKey(KeyCode.LeftArrow) && isInAir == false)
         {
             myRigidbody.velocity = Vector2.left * runSpeed;
             mySpriteRenderer.flipX = true;
             isTurnedLeft = true;
         }
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && isInAir == false)
         {
             myRigidbody.velocity = Vector2.up * jumpSpeed;
+            isInAir = true;
+            
         }
+
+
+
     }
 }
